542c8270dd9780164b2b4e9d85be615623091d1e
Vladimir <vladimirapolaiko@gmail.com>
2015-12-31 20:28:58 +0300

    Default server port

--- src/main/java/org/servicestation/Main.java
+++ src/main/java/org/servicestation/Main.java
@@ -5,12 +5,15 @@ import org.eclipse.jetty.webapp.WebAppContext;
 
 public class Main {
     private static final String RESOURCE_BASE = "src/main/webapp/";
+    private static final String DEFAULT_SERVER_PORT = "5000";
 
     public static void main(String[] args) throws Exception {
 
+
+
         String webPort = System.getenv("PORT");
         if (webPort == null || webPort.isEmpty()) {
-            webPort = "5000";
+            webPort = DEFAULT_SERVER_PORT;
         }
 
         Server server = new Server(Integer.valueOf(webPort));
