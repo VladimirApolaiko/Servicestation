99235c79d6f1ddcf6894b37c4d1d1dc57c94af54
Vladimir <vladimirapolaiko@gmail.com>
2015-12-31 21:03:53 +0300

    Changes in hello message

--- src/main/java/org/servicestation/resources/Hello.java
+++ src/main/java/org/servicestation/resources/Hello.java
@@ -6,15 +6,13 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-/**
- * Created by Vladimir on 31.12.2015.
- */
+
 @Path("/")
 public class Hello {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String sayHello() {
-        return "Hello Niculcha Vlad it's our firs step to create great app which will use all people of world";
+        return "Hello Niculcha Vlad it's our firs step to create great app which will use all people of world !!!!";
     }
 }
