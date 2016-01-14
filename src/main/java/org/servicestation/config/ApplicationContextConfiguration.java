5e1cb278a5e915ec498ef0933699ac9994997fcd
Vladimir <vladimirapolaiko@gmail.com>
2016-01-14 18:40:30 +0300

    some bug fixing

--- src/main/java/org/servicestation/config/ApplicationContextConfiguration.java
+++ src/main/java/org/servicestation/config/ApplicationContextConfiguration.java
@@ -4,7 +4,6 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.servicestation.dao.impl.UserDaoImpl;
 import org.servicestation.resources.impl.HelloResourceImpl;
 import org.servicestation.resources.impl.UserResourceImpl;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -17,14 +16,14 @@ public class ApplicationContextConfiguration {
 
     private static final Integer INITIAL_POOL_SIZE = 10;
 
-    @Value("${DATABASE_URL}")
-    private String DATABASE_URL;
+    /* @Value("${DATABASE_URL}")*/
+    private String DATABASE_URL = System.getenv("DATABASE_URL");
 
-    @Value("${DATABASE_USERNAME}")
-    private String DATABASE_USERNAME;
+    /*@Value("${DATABASE_USERNAME}")*/
+    private String DATABASE_USERNAME = System.getenv("DATABASE_USERNAME");
 
-    @Value("${DATABASE_PASSWORD}")
-    private String DATABASE_PASSWORD;
+    /*@Value("${DATABASE_PASSWORD}")*/
+    private String DATABASE_PASSWORD = System.getenv("DATABASE_PASSWORD");
 
     @Bean
     public BasicDataSource basicDataSource() throws URISyntaxException {
--- src/main/java/org/servicestation/dao/impl/UserDaoImpl.java
+++ src/main/java/org/servicestation/dao/impl/UserDaoImpl.java
@@ -21,3 +21,4 @@ public class UserDaoImpl implements IUserDao {
         }, id);
     }
 }
+
