12167b4c2fc256d8b7e19cefa8a54c64445a9fa9
vladimir <vladimirapolaiko@gmail.com>
2016-05-28 00:00:28 +0300

    Dto mapper

--- src/main/java/org/servicestation/config/app/ApplicationContextConfiguration.java
+++ src/main/java/org/servicestation/config/app/ApplicationContextConfiguration.java
@@ -1,40 +1,16 @@
 package org.servicestation.config.app;
 
 import org.apache.commons.dbcp2.BasicDataSource;
-import org.apache.velocity.app.VelocityEngine;
-import org.eclipse.jetty.websocket.jsr356.server.AnnotatedServerEndpointConfig;
-import org.eclipse.jetty.websocket.jsr356.server.ServerContainer;
-import org.servicestation.dao.*;
-import org.servicestation.dao.impl.*;
-import org.servicestation.resources.ICarResource;
-import org.servicestation.resources.IEmailVerificationResource;
-import org.servicestation.resources.IPasswordRecoveryResource;
-import org.servicestation.resources.impl.*;
-import org.servicestation.resources.managers.*;
-import org.servicestation.resources.managers.impl.*;
-import org.servicestation.resources.managers.impl.MailManager;
-import org.servicestation.resources.sokets.WebSocketEventEmitter;
-import org.servicestation.resources.sokets.WebSocketExample;
-import org.servicestation.resources.sokets.handlers.GetAllOrdersWebSocketEventHandler;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.Scope;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
-import javax.annotation.PostConstruct;
-import javax.websocket.DeploymentException;
-import javax.websocket.server.ServerEndpoint;
-import javax.websocket.server.ServerEndpointConfig;
-import java.net.URI;
 import java.net.URISyntaxException;
 
 @Configuration
---
+++ src/main/java/org/servicestation/resources/mappers/impl/CarDtoMapper.java
@@ -0,0 +1,46 @@
+package org.servicestation.resources.mappers.impl;
+
+import org.servicestation.model.Car;
+import org.servicestation.resources.dto.CarDto;
+import org.servicestation.resources.mappers.IDtoMapper;
+
+public class CarDtoMapper implements IDtoMapper<CarDto, Car>{
+
+    @Override
+    public Car mapDtoToServerObject(CarDto dto) {
+        Car car = new Car();
+        car.id = dto.id;
+        car.brand = dto.brand;
+        car.model = dto.model;
+        car.engine_volume = dto.engine_volume;
+        car.vin = dto.vin;
+        car.registration_number = dto.registration_number;
+        car.username = dto.username;
+
+        return car;
+    }
+
+    @Override
+    public CarDto mapServerObjectToDto(Car serverObj) {
+        CarDto carDto = new CarDto();
+        carDto.id = serverObj.id;
+        carDto.brand = serverObj.brand;
+        carDto.model = serverObj.model;
+        carDto.engine_volume = serverObj.engine_volume;
+        carDto.vin = serverObj.vin;
+        carDto.registration_number = serverObj.registration_number;
+        carDto.username = serverObj.username;
+
+        return carDto;
+    }
+
+    @Override
+    public Class getDtoType() {
+        return CarDto.class;
+    }
+
+    @Override
+    public Class getServerObjectType() {
+        return Car.class;
+    }
+}
